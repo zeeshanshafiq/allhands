c6cb757000a9ba4c2d1263148a28d14388996137
Han Yuan <hanyuan@ebay.com>
2014-05-11 14:45:00 -0700

    Added locked attribute to event.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140505131114) do
+ActiveRecord::Schema.define(version: 20140511213854) do
 
   create_table "events", force: true do |t|
     t.text     "description"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140505131114) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "headline"
+    t.boolean  "locked"
   end
 
   create_table "questions", force: true do |t|
