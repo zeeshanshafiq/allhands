7e3e6093090526242caac3abacca9487e924ebc6
Han Yuan <han.yuan@gmail.com>
2015-10-04 23:18:59 +0000

    Upgraded rails and localized custom parts so that everything is in resource bundles

--- Gemfile
+++ Gemfile
@@ -278,7 +278,7 @@ end
 
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '4.1.0'
+gem 'rails', '>=4.2.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use SCSS for stylesheets
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,6 @@
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( simplex.css )
 Rails.application.config.assets.precompile += %w( simplex.js )
+Rails.application.config.assets.precompile += %w( markitup/sets/markdown/h1.png )
+Rails.application.config.assets.precompile += %w(*.svg *.eot *.woff *.ttf *.gif *.png *.ico)
+Rails.application.config.assets.precompile << /\A(?!active_admin).*\.(js|css)\z/
