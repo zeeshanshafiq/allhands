0e8891a38902c1f4089de1d617bd9ea7b239b115
Han-Shen Yuan <han.yuan@gmail.com>
2019-01-04 11:53:26 -0800

    Repaired problems with stylesheets due to bootswatch reset

--- app/assets/javascripts/simplex.js
+++ app/assets/javascripts/simplex.js
@@ -2,5 +2,13 @@
 // Bootswatch
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-datepicker
+
 //= require simplex/loader
 //= require simplex/bootswatch
+
+//= require markitup
+//= require markitup/sets/xbbcode/set
+
+//= require events
